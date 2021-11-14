dd31f52e795bfcbca491a3b44b468bb596267066
Juni_trianto <triantoj13@gmail.com>
2021-11-14 12:28:12 +0700

    update-bug 1.56

---
+++ src/components/Pages/Home/Favorite.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Favorite = () => {
+  return (
+    <>
+      <span>
+        <i class="bi bi-heart-fill"></i>
+      </span>
+    </>
+  );
+};
+
+export default Favorite;
